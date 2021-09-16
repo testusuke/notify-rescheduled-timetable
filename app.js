3027169a0889b77cbf19921ec4e2733cf123b2bf
testusuke <silverbro007@icloud.com>
2021-09-16 11:53:40 +0900

    [Update] add mysql

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DB
+});
 
 app.get('/index', (req, res) => {
     res.render('index.ejs');
