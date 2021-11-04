1a4cca66123222291e877244466bf758f2c3c87a
testusuke <silverbro007@icloud.com>
2021-11-05 02:04:44 +0900

    change: add some functions

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const HOST = '127.0.0.1';
+const PORT = 3000;
+
 const express = require('express');
 const mysql = require('mysql');
+const e = require("express");
+// const session = require('express-session');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -12,8 +18,86 @@ const connection = mysql.createConnection({
     database: process.env.MYSQL_DB
 });
 
-app.get('/index', (req, res) => {
-    res.render('index.ejs');
+//  initialize tables
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('success connect db');
+    const sql = "create table if not exists tables\n" +
+        "(\n" +
+        "\tid int auto_increment,\n" +
+        "\tname VARCHAR(64) null,\n" +
+        "\tdescription VARCHAR(128) null,\n" +
+        "\tconstraint tables_pk\n" +
+        "\t\tprimary key (id)\n" +
+        ")";
+    //  create table
+    connection.query(
+        sql,
+        (err, result) => {
+            if (err) throw err
+            console.log('success create table');
+        }
+    );
 });
 
-app.listen(3000);
+//  redirect
+app.get('/', (req, res) => {
+    res.redirect('/index');
+});
+
+app.get('/index', (req, res) => {
+    connection.query(
+        'SELECT * FROM tables',
+        (error, results) => {
+            console.log(results);
+            res.render('index.ejs', {tables: results});
+        }
+    );
+});
+
+// TODO parameterにidを追加して、複数のtableを表示する
+app.get('/table/view/:id', (req, res) => {
+    res.render('table.ejs');
+});
+
+/**
+ * @description Creating Table Menu
+ */
+app.get('/table/create', (req, res) => {
+    res.render('table-creator.ejs');
+});
+
+/**
+ * @description Create Table
+ */
+app.post('/table/create', (req, res) => {
+    console.log('receive create table request');
+    connection.query(
+        'INSERT INTO tables (name, description) VALUES (?, ?)',
+        [req.body.name, req.body.description],
+        (error, results) => {
+            if (error) throw error
+            res.redirect('/index');
+        }
+    );
+});
+
+/**
+ * @description Delete Table
+ */
+app.post('/table/delete/:id', (req, res) => {
+    connection.query(
+        'DELETE FROM tables WHERE id=?',
+        [req.params.id],
+        (error, results) => {
+            if (error) throw error
+            res.redirect('/index');
+        }
+    );
+});
+
+//  start app
+app.listen(PORT, HOST, () => {
+    //  output url
+    console.log(`url: http://${HOST}:${PORT}`);
+});
